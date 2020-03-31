a8d592088f9b148cee69740db9d939c88137c257
Diego <dbarraza@disc.ucn.cl>
2020-03-31 12:43:34 -0300

    feat(add and implement): add the seeder and implement a controller
    add the seeder as ConversacionReply and Conversacion for teste the chat

--- chatDisc/app/Http/Controllers/ConversacionReplyController.php
+++ chatDisc/app/Http/Controllers/ConversacionReplyController.php
@@ -4,7 +4,74 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\ConversacionReply;
+
+use App\Http\Controllers\Controller;
+
+use DB;
+
 class ConversacionReplyController extends Controller
 {
+
+    public function createConversacionReply(Request $request){
+
+        $conversacionReply = ConversacionReply::create($request->all());
+
+        return response()->json($conversacionReply);
+
+    }
+
+    public function updateConversacionReply(Request $request, $id){
+
+
+
+        $conversacionReply  = DB::table('conversacion_reply')->where('id',$request->input('id'))->get();
+
+        $conversacionReply->reply = $request->input('reply');
+
+        $conversacionReply->ip = $request->input('ip');
+
+        $conversacionReply->time = $request->input('time');
+
+        $conversacionReply->latitude = $request->input('latitude');
+
+        $conversacionReply->logitude = $request->input('logitude');
+
+        $conversacionReply->save();
+
+        $response["conversacionReplys"] = $conversacionReply;
+
+        $response["success"] = 1;
+
+        return response()->json($response);
+
+    }
+
+
+
+    public function deleteConversacionReply($id , Request $request){
+
+        $conversacionReply  = DB::table('conversacion_reply')->where('id',$request->input('id'))->get();
+
+        $conversacionReply->delete();
+
+        return response()->json('Removed successfully.');
+
+    }
+
+
+
+    public function index(){
+
+        $conversacionReply  = ConversacionReply::all();
+
+        $response["conversacionReplys"] = $conversacionReply;
+
+        $response["success"] = 1;
+
+        return response()->json($response);
+
+    }
+
 
 }
